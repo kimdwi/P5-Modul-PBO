8ab8650b19bacf5a5f1d19a4b62b32e92f7d2aab
kimdwi <parkdwiputriani@gmail.com>
2022-04-18 00:10:45 +0700

    Asosiasi

---
+++ src/asosiasi/TestDriver.java
@@ -0,0 +1,9 @@
+package asosiasi;
+
+public class TestDriver {
+    public static void main(String[] args) {
+        //Membuat Objek
+        Driver obj = new Driver("Andy", "Ford", 9988);
+        System.out.println(obj.driverName+ "Is A Drive Of Car Id : " +obj.carId);
+    }
+}
